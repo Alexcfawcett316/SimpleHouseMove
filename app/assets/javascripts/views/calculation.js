550678986875e244a08e3d57c6ef25b18c0e801b
Alex Fawcett <alexfawcett@outlook.com>
2014-05-13 21:43:21 +0100

    Fixed issue with slider params changing and added functionality to calculation model

--- app/assets/javascripts/views/calculation.js
+++ app/assets/javascripts/views/calculation.js
@@ -1,14 +1,43 @@
 var MgCalculation = Backbone.View.extend({
 
-  el: '#calculatorMain', 
+  el: '#calculatorMain',
+  model: new CalculationModel, 
  
   initialize: function() {
-	$( ".defaultSlider" ).slider({
+	var t = this;
+	var houseSellMax = 2000000;
+	var houseSellStep = 10000;
+	$( "#sliderToBuy" ).slider({
 			range: "min",
      			min: 0,
-      			max: 2000000,
-			step: 10000,
+      			max: houseSellMax,
+			step: houseSellStep,
+			slide: function( event, ui ) {
+				t.onSliderBuyChanged(ui.value);
+			}
 			});
+	
+	$( "#sliderPayFor" ).slider({
+			range: "min",
+     			min: 0,
+      			max: houseSellMax,
+			step: houseSellStep,
+			slide: function( event, ui ) {
+				t.onSliderPayForChanged(ui.value);
+			}
+			});
+
+	$( "#sliderSellFor" ).slider({
+			range: "min",
+     			min: 0,
+      			max: houseSellMax,
+			step: houseSellStep,
+			slide: function( event, ui ) {
+				t.onSliderSellForChanged(ui.value);
+			}
+			});
+	
+
 		
 	$( "#sliderSolicitor" ).slider({
 			range: "min",
@@ -16,6 +45,7 @@ var MgCalculation = Backbone.View.extend({
      			min: 0,
       			max: 0
 			});
+	
 	$( "#sliderEstateAgent" ).slider({
 			range: "min",
 			value: 0,
@@ -27,21 +57,33 @@ var MgCalculation = Backbone.View.extend({
 			range: "min",
 			value: 0,
      			min: 0,
-      			max: 5000
+      			max: 5000,
+			slide: function( event, ui ) {
+				t.onSlideExtras(ui.value);
+			}
+
 			});
 
 	$( "#sliderTakenOver" ).slider({
 			range: "min",
 			value: 35,
      			min: 0,
-      			max: 35
+      			max: 35,
+		        slide: function( event, ui ) {
+				t.onSlideTakenOver(ui.value);
+			}
+
 			});
 	
 	$( "#sliderInterestRate" ).slider({
 			range: "min",
 			value: 20,
      			min: 0,
-      			max: 100
+      			max: 100,
+			slide: function( event, ui ) {
+				t.onSlideInterestRate(ui.value);
+			}
+
 			});
 
 
@@ -52,24 +94,14 @@ var MgCalculation = Backbone.View.extend({
    },
 
    events: {
-	"slide #sliderToBuy" : "onSliderBuyChanged",
 	"click #rdSellNo" : "onSellRadioChanged",
-	"click #rdSellYes" : "onSellRadioChanged",
-    	"slide #sliderPayFor" : "onSliderPayForChanged",
-	"slide #sliderSellFor" : "onSliderSellForChanged",
-	"slide #sliderSolicitor" : "onSliderSolicitor",
-	"slide #sliderEstateAgent" : "onSliderEstateAgent",
-	"slide #sliderExtraCosts" : "onSlideExtras",
-	"slide #sliderTakenOver" : "onSlideTakenOver",
-	"slide #sliderInterestRate" : "onSlideInterestRate"
-
-
-	
+	"click #rdSellYes" : "onSellRadioChanged"	
      },
 
-   onSliderBuyChanged: function(){
-   	$('#lblHowMuchBuy').text("£" + $('#sliderToBuy').slider("value"));
-	this.adjustFees();	
+   onSliderBuyChanged: function(amount){
+	this.model.set("amountToBuy", amount);
+   	$('#lblHowMuchBuy').text("£" + amount);
+	this.adjustSolicitorFees();	
    },
 
   onSellRadioChanged: function() {
@@ -78,72 +110,93 @@ var MgCalculation = Backbone.View.extend({
 		sellPanel.fadeOut( "fast", function() {
   			
   		});
+		this.model.set("houseToSell", false);
 	}	
 	else{
 		sellPanel.fadeIn( "fast", function() {
   			
   		});
+		this.model.set("houseToSell", true);
+
 	}
 
   },
 
-  onSliderPayForChanged: function (){
-	$('#lblHowMuch').text("£" + $('#sliderPayFor').slider("value"));	  
+  onSliderPayForChanged: function (amount){
+	this.model.set("howMuchPayFor", amount);
+	$('#lblHowMuch').text("£" + amount);	  
   },
 
-  onSliderSellForChanged: function (){
-	$('#lblSellFor').text("£" + $('#sliderSellFor').slider("value"));	  
+  onSliderSellForChanged: function (amount){
+	this.model.set("howMuchSellFor", amount);
+	$('#lblSellFor').text("£" + amount);	
+	this.adjustEstateAgentFees();
   },
   
-  onSliderSolicitor: function (){
-	$('#lblSolVal').text("£" + $('#sliderSolicitor').slider("value"));	
+  onSliderSolicitor: function (amount){
+	$('#lblSolVal').text("£" + amount);	
   },
 
-  onSliderEstateAgent: function (){
-	$('#lblEstateAgent').text("£" + $('#sliderEstateAgent').slider("value"));	
+  onSliderEstateAgent: function (amount){
+	$('#lblEstateAgent').text("£" + amount);	
   },
 
 
-  onSlideExtras :function(){
-  	$('#lblExtraCosts').text("£" + $('#sliderExtraCosts').slider("value"));	
+  onSlideExtras :function(amount){
+  	$('#lblExtraCosts').text("£" + amount);	
   },
 
-  onSlideTakenOver :function(){
-  	$('#lblTakenOver').text($('#sliderTakenOver').slider("value") + " years");	
+  onSlideTakenOver :function(amount){
+  	$('#lblTakenOver').text(amount + " years");	
   },
 
-  onSlideInterestRate :function(){
-	var val = $('#sliderInterestRate').slider("value") / 10;
+  onSlideInterestRate :function(amount){
+	var val = amount / 10;
   	$('#lblInterestRate').text(val + "%");	
   },
 
 
   
 
-  adjustFees:function(){
-  	  var middle = $('#sliderToBuy').slider("value") * 0.01;
-	  var min = middle - middle;
-  	  var max = middle + middle;
-	  
+  adjustSolicitorFees:function(){
+  	  var middle = this.model.solicitorsFee();
+	  var min = 0;
+  	  var max = middle * 2;
+	  var t = this;
   	  $( "#sliderSolicitor" ).slider({
 			range: "min",
 			value: middle,
      			min: min,
-      			max: max
-			});
-	  $('#lblSolVal').text("£" + middle);
+      			max: max,
+		  	slide: function( event, ui ) {
+				t.onSliderSolicitor(ui.value);
+			}
 
-	  $( "#sliderEstateAgent" ).slider({
+			});
+	  
+	  
+	  $('#lblSolVal').text("£" + middle);
+   },
+   
+   adjustEstateAgentFees:function(){
+  	  var middle = this.model.estateAgentFee();
+	  var min = 0;
+  	  var max = middle * 2;
+	  var t = this;
+  	  $( "#sliderEstateAgent" ).slider({
 			range: "min",
 			value: middle,
      			min: min,
-      			max: max
+      			max: max,
+		  	slide: function( event, ui ) {
+				t.onSliderEstateAgent(ui.value);
+			}
+
 			});
+	  
+	  
 	  $('#lblEstateAgent').text("£" + middle);
-
-
-
-  }  
+   }   
 
 
 });
